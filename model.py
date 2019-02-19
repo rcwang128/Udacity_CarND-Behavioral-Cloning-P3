2b83bb6eebc38c6fcc32351a76417821a5cc377f
rcwang128 <wanghlck@gmail.com>
2019-02-19 05:35:08 +0000

    add model.py

---
+++ model.py
@@ -0,0 +1,51 @@
+import numpy as np
+import csv
+import cv2
+from matplotlib import pyplot as plt
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Lambda, Cropping2D, Convolution2D, Flatten, Dense
+
+lines = []
+images = []
+steering = []
+
+
+with open('data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+# Delet first title line
+del lines[0]
+#print(lines[0])
+
+for line in lines:
+    file_name = line[0].split('/')[-1]
+    #print (file_name)
+    image_path = 'data/IMG/' + file_name
+    image = cv2.imread(image_path)
+    images.append(image)
+    steering.append(float(line[3]))
+
+X_train = np.array(images)
+y_train = np.array(steering)
+
+model = Sequential()
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((65,25),(0,0))))
+model.add(Convolution2D(24,5,5,subsample=(2,2), activation='relu'))
+model.add(Convolution2D(36,5,5,subsample=(2,2), activation='relu'))
+model.add(Convolution2D(48,5,5,subsample=(2,2), activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
+
+model.save('model.h5')
+
