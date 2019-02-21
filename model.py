4a398e8d7eccf3b91cab61c008f706221b160ba1
HarryWang <wanghlck@gmail.com>
2019-02-21 03:35:32 +0000

    model updated

--- model.py
+++ model.py
@@ -4,14 +4,14 @@ import cv2
 from matplotlib import pyplot as plt
 import tensorflow as tf
 from keras.models import Sequential
-from keras.layers import Lambda, Cropping2D, Convolution2D, Flatten, Dense
+from keras.layers import Lambda, Cropping2D, Convolution2D, Flatten, Dense, Dropout
 
 lines = []
 images = []
 steering = []
 
 
-with open('data/driving_log.csv') as csvfile:
+with open('myData/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
         lines.append(line)
@@ -23,7 +23,7 @@ del lines[0]
 for line in lines:
     file_name = line[0].split('/')[-1]
     #print (file_name)
-    image_path = 'data/IMG/' + file_name
+    image_path = 'myData/IMG/' + file_name
     image = cv2.imread(image_path)
     images.append(image)
     steering.append(float(line[3]))
@@ -43,10 +43,11 @@ model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
+model.add(Dropout(0.5))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
 
