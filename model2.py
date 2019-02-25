036ddb9f1e79c2d835e5bc709a3503bbdad54729
harry <wanghlck@gmail.com>
2019-02-25 23:35:36 +0000

    update with right image

--- model2.py
+++ model2.py
@@ -36,12 +36,26 @@ def generator(samples, batch_size=32):
             steering = []
             for batch_sample in batch_samples:
                 file_name = data_path + '/IMG/' + batch_sample[0].split('/')[-1]
+                file_name1 = data_path + '/IMG/' + batch_sample[1].split('/')[-1]
+                file_name2 = data_path + '/IMG/' + batch_sample[2].split('/')[-1]
+
                 center_image = cv2.cvtColor(cv2.imread(file_name), cv2.COLOR_BGR2RGB)
-                #center_iamge = center_image.squeeze()	
-                #center_image = center_image[65:135, 0:320]
-                center_steering = float(batch_sample[3])
                 images.append(center_image)
+                #left_image = cv2.cvtColor(cv2.imread(file_name1), cv2.COLOR_BGR2RGB)
+                #images.append(left_image)
+                right_image = cv2.cvtColor(cv2.imread(file_name2), cv2.COLOR_BGR2RGB)
+                images.append(right_image)
+
+                center_steering = float(batch_sample[3])
                 steering.append(center_steering)
+                steering_coef = 0.2
+                #left_steering = center_steering + steering_coef
+                #steering.append(left_steering)
+                right_steering = center_steering - steering_coef
+                steering.append(right_steering)
+                
+                #images.append(center_image)
+                #steering.append(center_steering)
                 
                 # Flip image/measurement
                 images.append(cv2.flip(center_image, 1))
@@ -64,17 +78,19 @@ model.add(Convolution2D(36,5,5,subsample=(2,2), activation='relu'))
 model.add(Convolution2D(48,5,5,subsample=(2,2), activation='relu'))
 model.add(Convolution2D(64,3,3,activation='relu'))
 model.add(Convolution2D(64,3,3,activation='relu'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(100, activation='relu'))
-#model.add(Dropout(0.5))
+model.add(Dropout(0.5))
 model.add(Dense(50, activation='relu'))
+model.add(Dropout(0.5))
 model.add(Dense(10, activation='relu'))
+model.add(Dropout(0.5))
 model.add(Dense(1))
 adam = optimizers.Adam(lr=0.0001)
 model.compile(loss='mse', optimizer=adam, metrics=['mae','acc'])
 
 model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
 
-model.save('model2_test.h5')
+model.save('model2_test3.h5')
 
