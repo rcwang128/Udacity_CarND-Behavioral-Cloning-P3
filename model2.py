ba9a01b448cb381bd27bbeabfa5286fad45160cf
Harry Wang <wanghlck@gmail.com>
2019-02-25 19:42:50 +0000

    update model2.py and a test .h5

--- model2.py
+++ model2.py
@@ -2,13 +2,15 @@ import numpy as np
 import csv
 import cv2
 import tensorflow as tf
+from random import shuffle
 from keras.models import Sequential
 from keras.layers import Lambda, Cropping2D, Convolution2D, Flatten, Dense, Dropout
+from keras import optimizers
 from sklearn.model_selection import train_test_split 
 import sklearn
 
 
-data_path = 'data_2laps'
+data_path = 'data'
 
 samples = []
 
@@ -17,6 +19,8 @@ with open(data_path + '/driving_log.csv') as csvfile:
     for line in reader:
         samples.append(line)
 csvfile.close()
+
+del samples[0]
 
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
@@ -33,6 +37,8 @@ def generator(samples, batch_size=32):
             for batch_sample in batch_samples:
                 file_name = data_path + '/IMG/' + batch_sample[0].split('/')[-1]
                 center_image = cv2.cvtColor(cv2.imread(file_name), cv2.COLOR_BGR2RGB)
+                #center_iamge = center_image.squeeze()	
+                #center_image = center_image[65:135, 0:320]
                 center_steering = float(batch_sample[3])
                 images.append(center_image)
                 steering.append(center_steering)
@@ -45,29 +51,30 @@ def generator(samples, batch_size=32):
             y_train = np.array(steering)
             yield sklearn.utils.shuffle(X_train, y_train)
 
-    print(len(X_train)); print(len(y_train))
+    #print(len(X_train)); print(len(y_train))
 
 train_generator = generator(train_samples, batch_size=32)
 validation_generator = generator(validation_samples, batch_size=32)
 
 model = Sequential()
-model.add(Lambda(lambda x: (x / 255) - 0.5, input_shape=(70,320,3)))
-#model.add(Cropping2D(cropping=((70,25),(0,0))))
-model.add(Convolution2D(24,5,5,subsample=(2,2), activation='elu'))
-model.add(Convolution2D(36,5,5,subsample=(2,2), activation='elu'))
-model.add(Convolution2D(48,5,5,subsample=(2,2), activation='elu'))
-model.add(Convolution2D(64,3,3,activation='elu'))
-model.add(Convolution2D(64,3,3,activation='elu'))
-#model.add(Dropout(0.5))
+model.add(Lambda(lambda x: (x / 255) - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((65,25),(0,0))))
+model.add(Convolution2D(24,5,5,subsample=(2,2), activation='relu'))
+model.add(Convolution2D(36,5,5,subsample=(2,2), activation='relu'))
+model.add(Convolution2D(48,5,5,subsample=(2,2), activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Dropout(0.5))
 model.add(Flatten())
-model.add(Dense(100, activation='elu'))
-model.add(Dense(50, activation='elu'))
-model.add(Dense(10, activation='elu'))
+model.add(Dense(100, activation='relu'))
+#model.add(Dropout(0.5))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(10, activation='relu'))
 model.add(Dense(1))
-
-model.compile(loss='mse', optimizer='adam')
+adam = optimizers.Adam(lr=0.0001)
+model.compile(loss='mse', optimizer=adam, metrics=['mae','acc'])
 
 model.fit_generator(train_generator, samples_per_epoch=len(train_samples), validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=3)
 
-model.save('model2.h5')
+model.save('model2_test.h5')
 
